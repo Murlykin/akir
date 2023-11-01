c0746ae5d103c41fe33d75dd2019c75a3e41639f
Andrii Murlykin <109280143+Murlykin@users.noreply.github.com>
2023-11-01 15:10:20 +0200

    let's start working

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a> */}
       </div>
       <h1>Сайт АКІР</h1>
+      <p>Start working</p>
       {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
